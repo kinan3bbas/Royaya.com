7c5db8b21db37163c1d825806308b19a7366b9d1
kinan Abbas <kinan.magnamedia@gmail.com>
2020-01-21 18:25:30 +0200

    Main Pages

--- RoyayaControlPanel.com/Controllers/HomeController.cs
+++ RoyayaControlPanel.com/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Royaya.com.Models;
+using RoyayaControlPanel.com.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +13,7 @@ namespace RoyayaControlPanel.com.Controllers
     [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             return View();
@@ -24,6 +29,41 @@ namespace RoyayaControlPanel.com.Controllers
         public ActionResult Main()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Dream dream =  db.Dreams.Find(id);
+            if (dream == null)
+            {
+                return HttpNotFound();
+            }
+
+            dream.numberOfViews++;
+            db.Entry(dream).State = EntityState.Modified;
+            db.SaveChanges();
+
+            ApplicationUser creator = db.Users.Where(a => a.Id.Equals(dream.Creator)).FirstOrDefault();
+            ApplicationUser Interpreter = db.Users.Where(a => a.Id.Equals(dream.interpretatorId)).FirstOrDefault();
+            InterprationPath path = db.InterprationPaths.Where(a => a.id.Equals(dream.pathId)).FirstOrDefault();
+            if (path.Cost == 0)
+                ViewBag.Type = "رؤية مجانية";
+            else
+                ViewBag.Type = "رؤية مدفوعة";
+
+            ViewBag.InterpreterName = Interpreter != null ? Interpreter.Name : "";
+            ViewBag.CreatorName = creator != null ? creator.Name : "";
+            return View(dream);
         }
 
         public ActionResult About()
