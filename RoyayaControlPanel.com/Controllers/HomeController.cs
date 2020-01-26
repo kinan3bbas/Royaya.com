09ad6d1f27063d8ddbb0831cefcee074a1c67351
kinan Abbas <kinan.magnamedia@gmail.com>
2020-01-26 18:25:12 +0200

    Serach

--- RoyayaControlPanel.com/Controllers/HomeController.cs
+++ RoyayaControlPanel.com/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Royaya.com.Models;
+﻿using PagedList;
+using Royaya.com.Models;
 using RoyayaControlPanel.com.Models;
+using RoyayaControlPanel.com.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -66,6 +68,49 @@ namespace RoyayaControlPanel.com.Controllers
             return View(dream);
         }
 
+
+        [AllowAnonymous]
+        public JsonResult GetDreams(int? page, string searchString, string status)
+        {
+            var users = db.Users;
+            var dreams = db.Dreams.Include(d => d.interpretator).Include(d => d.path).OrderByDescending(r => r.CreationDate);
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            if (searchString != null && !searchString.Equals(""))
+                dreams = dreams.Where(a => a.Description.Contains(searchString)).OrderByDescending(r => r.CreationDate);
+            if (status != null && !status.Equals(""))
+                dreams = dreams.Where(a => a.Status.Equals(status)).OrderByDescending(r => r.CreationDate);
+            //dreams = dreams.Skip((pageNumber-1)*pageSize).Take(pageSize).ToList();
+            List<DreamViewModel> dreamsView = new List<DreamViewModel>();
+            foreach (var item in dreams)
+            {
+                DreamViewModel temp = new DreamViewModel
+                {
+                    id = item.id,
+                    CreatorId = item.Creator,
+
+                    //temp.CreatorName = users.Where(a => a.Id.Equals(item.Creator)).FirstOrDefault()!=null?users.Where(a => a.Id.Equals(item.Creator)).FirstOrDefault().Name:"";
+                    Description = item.Description,
+                    Explanation = item.Explanation,
+                    ExplanationDate = item.ExplanationDate,
+                    InterpretationStartDate = item.InterpretationStartDate,
+                    interpretatorId = item.interpretatorId != null ? item.interpretatorId : "",
+                    interpretatorName = item.interpretator != null ? item.interpretator.Name : "",
+                    pathCost = item.path != null ? item.path.Cost : -1,
+                    pathId = item.pathId != null ? item.pathId : 0,
+                    RatingDate = item.RatingDate,
+                    Status = item.Status,
+                    UserRating = item.UserRating,
+                    CreationDate = item.CreationDate,
+                    numberOfLikes = item.numberOfLikes,
+                    numberOfViews = item.numberOfViews,
+
+                    AllItems = dreams.Count()
+                };
+                dreamsView.Add(temp);
+            }
+            return Json(dreamsView.ToPagedList(pageNumber, pageSize), JsonRequestBehavior.AllowGet);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
